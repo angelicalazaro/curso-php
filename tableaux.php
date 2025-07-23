603d2e4578e61966814baa163b11a49416ec1ea6
Angelica Lazaro <angelicamld@gmail.com>
2025-07-23 11:11:14 +0200

    creation of arrays

---
+++ tableaux.php
@@ -0,0 +1,34 @@
+<?php
+$eleve = [
+    'nom' => 'Lazaro', 
+    'prenom' => 'Angelica', 
+    'notes' => [10, 20, 15]
+];
+// replace the name 
+$eleve['prenom'] = 'Marie';
+// adding a new note
+$eleve['notes'][3] = 18;
+// Displaying the student's full name
+echo $eleve['prenom'] . ' ' . $eleve['nom'];
+// Displaying the array of notes
+print_r($eleve['notes']);
+// An array in an array
+$classe = [
+    [
+        'nom' => 'Lazaro', 
+        'prenom' => 'Angelica', 
+        'notes' => [10, 20, 15]
+    ],
+    [
+        'nom' => 'Dupont', 
+        'prenom' => 'Jean', 
+        'notes' => [12, 14, 16]
+    ],
+    [
+        'nom' => 'Martin', 
+        'prenom' => 'Sophie', 
+        'notes' => [18, 19, 17]
+    ]
+];
+// displaying second student's first note
+echo $classe[1]['notes'][0];
